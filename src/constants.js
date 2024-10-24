2d6fac55120c198a3c373c5bc3ff44bb4ec85ce1
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 19:07:40 +0600

    constant values added

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+export const UserRoleEnum = {
+  jobseeker: "jobseeker",
+  recruiter: "recruiter",
+}
+
+export const ApplicationStatusEnum = {
+  pending: "pending",
+  accepted: "accepted",
+  rejected: "rejected",
+}
+
+
+export const routes = {
+  jobs: "/jobs",
+  browse: "/browse",
+  profile: "/profile",
+  login: "/login",
+  signup: "/signup",
+  about: "/about",
+  contact: "/contact",
+  privacyPolicy: "/privacy-policy",
+  termsCondition: "/terms-condition",
+  recruiterCompanies: "/rc/company",
+  recruiterCreateCompany: "/rc/company/create",
+  recruiterEditCompany: "/rc/company/edit",
+  recruiterJobs: "/rc/jobs",
+  recruiterJobsByCompany: "/rc/jobs/company",
+}
