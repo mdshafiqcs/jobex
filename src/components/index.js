b8f9b331ea3b8906d307fb0d8be9d662eb6a9369
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 19:25:43 +0600

    components imported

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,15 @@ import PageNotFound from "./app/PageNotFound";
 import RecruiterHomeComponent from "./app/RecruiterHomeComponent";
 import UserHomeComponent from "./app/UserHomeComponent";
 import PageWrapper from "./app/PageWrapper";
+import Logo from "./app/Logo";
+import RoleProtectedRoute from "./app/RoleProtectedRoute";
 
 
 export {
+  Logo,
   PageNotFound,
   RecruiterHomeComponent,
   UserHomeComponent,
   PageWrapper,
+  RoleProtectedRoute,
 }
