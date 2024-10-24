d1409249303e0a41dd0b7a5a27cac22f054cecdf
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-24 21:43:34 +0600

    Header updated

--- src/components/app/header/Header.jsx
+++ src/components/app/header/Header.jsx
@@ -4,11 +4,23 @@ import Navbar from './Navbar'
 import AvatarButton from './AvatarButton'
 import AuthButton from './AuthButton'
 import { useSelector } from 'react-redux'
+import { Menu } from 'lucide-react'
+import { useRef } from 'react'
 
 
 function Header() {
 
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+  const menuRef = useRef(null);
+
+  function openMenu() {
+    menuRef.current.style.right = "0";
+  }
+
+  function closeMenu() {
+    menuRef.current.style.right = "-350px";
+  }
 
   return (
     <div className=' sticky w-full z-10 bg-white shadow-sm'>
@@ -18,6 +30,7 @@ function Header() {
           <nav>
             <div className='flex gap-12 items-center'>
               <Navbar/>
+              
               {
                 isLoggedIn ?  <AvatarButton/> :  <AuthButton/>
               }
