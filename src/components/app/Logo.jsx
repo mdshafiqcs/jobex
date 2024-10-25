c115cd2fe1739eab3dfd22ee4206e7a09a5a7cbd
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-25 22:27:41 +0600

    logo resized

--- src/components/app/Logo.jsx
+++ src/components/app/Logo.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Logo() {
   return (
     <Link to="/">
-      <div className='font-bold text-3xl text-slate-600 cursor-pointer'>Job<span className='text-orange-700'>Ex</span></div>
+      <div className='font-bold text-2xl sm:text-3xl text-slate-600 cursor-pointer'>Job<span className='text-orange-700'>Ex</span></div>
     </Link>
   )
 }
