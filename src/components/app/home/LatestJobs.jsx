da33a45e8932990a8837bfeeb13f03e49688b078
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-25 22:30:34 +0600

    latest jobs responsive done

--- src/components/app/home/LatestJobs.jsx
+++ src/components/app/home/LatestJobs.jsx
@@ -12,8 +12,8 @@ function LatestJobs() {
 
   return (
     <div className=' my-20'>
-      <h1 className='text-3xl font-bold text-slate-700'><span className='text-orange-700'>Latest</span> Job Opennings</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-5'>
+      <h1 className='text-2xl sm:text-3xl font-bold text-slate-700'><span className='text-orange-700'>Latest</span> Job Opennings</h1>
+      <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 my-5'>
         {
           jobs.slice(0,6).map((job) => (
             <JobCard key={job._id} job={job}/>
