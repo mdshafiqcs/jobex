a121b05ac312b5c46769c1b7cc49dc18bb298253
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-25 15:51:39 +0600

    search-job page updated

--- src/pages/SearchJob.jsx
+++ src/pages/SearchJob.jsx
@@ -1,4 +1,4 @@
-import { JobCard } from '@/components/app/jobs';
+import { FilterCard, JobCard } from '@/components/app/jobs';
 import { Button } from '@/components/ui/button';
 import { useGetJobs, useSearchJobs } from '@/hooks';
 import { addSearchedJobs, storeSearchQuery } from '@/store/jobSlice';
@@ -36,44 +36,51 @@ export default function SearchJob() {
 
 
   return (
-    <div className='mt-5'>
-
-      <div className='my-10 flex min-w-[400px] md:w-[70%] xl:w-[60%] h-[50px] shadow-lg border border-slate-200 pl-3 rounded-full items-center gap-4 mx-auto overflow-hidden'>
-        <input 
-        type="text" 
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={handleKeyDown}
-        value={query}
-        placeholder='search' 
-        className='outline-none border-none w-full px-3 text-slate-600' 
-        />
-        <Button 
-        onClick={searchJobHandler}
-        className=" bg-orange-700 hover:bg-orange-600 h-[50px] "
-        >
-          <Search className='h-5 w-5 '/>
-        </Button>
-      </div>
-
-        {loading ?
-        <div className='flex justify-center items-center h-80 mx-auto'>
-        <Loader2 className="mr-2 h-8 w-8 text-center animate-spin duration-1000"/>
+      <>
+        <div className='my-5 flex min-w-[400px] md:w-[70%] xl:w-[60%] h-[50px] shadow-lg border border-slate-200 pl-3 rounded-full items-center gap-4 mx-auto overflow-hidden'>
+          <input 
+          type="text" 
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
+          value={query}
+          placeholder='search' 
+          className='outline-none border-none w-full px-3 text-slate-600' 
+          />
+          <Button 
+          onClick={searchJobHandler}
+          className=" bg-orange-700 hover:bg-orange-600 h-[50px] "
+          >
+            <Search className='h-5 w-5 '/>
+          </Button>
         </div>
-        : 
-          jobs.length <= 0 ? <div className='text-center p-4'>No jobs found</div> :
-          <div className='flex-1 h-[88vh] overflow-y-auto p-4 pb-10 pt-0'>
-            <h1 className='mb-2 font-medium text-slate-700'>Search Result: ({paginateOption?.itemCount || 0})</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3'>
-              {
-                jobs.map((job) => (
-                  <div key={job._id}>
-                    <JobCard job={job}/>
-                  </div>
-                ))
-              }
-            </div>
+
+        <div className='flex gap-5 mx-auto '>
+          <div className='w-[100px] md:w-[200px]'>
+            <FilterCard />
           </div>
-        }
-    </div>
+
+          {loading ?
+            <div className='flex justify-center items-center h-80 mx-auto'>
+            <Loader2 className="mr-2 h-8 w-8 text-center animate-spin duration-1000"/>
+            </div>
+            : 
+              jobs.length <= 0 ? <div className='text-center p-4'>No jobs found</div> :
+              <div className='flex-1 h-[88vh] overflow-y-auto p-4 pb-10 pt-0'>
+                <h1 className='mb-2 font-medium text-slate-700'>Search Result: ({paginateOption?.itemCount || 0})</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3'>
+                  {
+                    jobs.map((job) => (
+                      <div key={job._id}>
+                        <JobCard job={job}/>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            }
+        </div>
+
+      </>
+
   )
 }
