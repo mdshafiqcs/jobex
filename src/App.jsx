a6c490a1465c5c3114af09ca80eef0baa7cdbde7
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-25 12:07:54 +0600

    react-qeuery added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import { Outlet } from "react-router-dom"
 import { Footer, Header } from "./components"
 import { Toaster } from "sonner"
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <div className='container mb-24'>
         <Outlet />
       </div>
       <Toaster />
       <Footer />
-      </>
+    </QueryClientProvider>
   )
 }
 
