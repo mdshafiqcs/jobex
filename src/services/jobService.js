4a3c0ed8b3fa825be38a76914878ccbdafdeb2bd
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-27 16:02:50 +0600

    asyncWrapper removed from jobService

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -2,62 +2,83 @@ import endpoints from "@/endpoints";
 import { asyncWrapper } from "@/utils";
 import axios from "axios";
 
-const getAllJobs = asyncWrapper(async(currentPage = 1, limit = 10, keyword = "") => {
-  const response = await axios.get(
-    endpoints.userAllJob(currentPage, limit, keyword), 
-    {
-      headers: {
-      'Accept': 'application/json',
-      }, 
-      withCredentials: true,
-    }
-  );
+const getAllJobs = async({currentPage = 1, limit = 9}) => {
 
-  return response.data.data;
-})
+  try {
+    const response = await axios.get(
+      endpoints.userAllJob({currentPage, limit}), 
+        {
+          headers: {
+          'Accept': 'application/json',
+          }, 
+          withCredentials: true,
+        }
+      );
+    
+      return response.data.data;
+  } catch (error) {
+    throw error.response.data; 
+  }
+}
 
-const getJobById = asyncWrapper(async(jobId) => {
+const getJobById = async(jobId) => {
 
-  const response = await axios.get(
-    endpoints.userJobById(jobId), 
-    {
-      headers: {
-      'Accept': 'application/json',
-      }, 
-      withCredentials: true,
-    }
-  );
+  try {
+    const response = await axios.get(
+      endpoints.userJobById(jobId), 
+      {
+        headers: {
+        'Accept': 'application/json',
+        }, 
+        withCredentials: true,
+      }
+    );
+  
+    return response.data.data;
 
-  return response.data.data;
-})
+  } catch (error) {
+    throw error.response.data; 
+  }
 
-const getAppliedJobs = asyncWrapper(async(currentPage = 1, limit = 10) => {
-  const response = await axios.get(
-    endpoints.userAppliedJobs(currentPage, limit), 
-    {
-      headers: {
-      'Accept': 'application/json',
-      }, 
-      withCredentials: true,
-    }
-  );
+}
 
-  return response.data.data;
-})
+const getAppliedJobs = async(currentPage = 1, limit = 10) => {
 
-const applyJob = asyncWrapper(async(jobId) => {
-  const response = await axios.get(
-    endpoints.userApplyJob(jobId), 
-    {
-      headers: {
-      'Accept': 'application/json',
-      }, 
-      withCredentials: true,
-    }
-  );
+  try {
+    const response = await axios.get(
+      endpoints.userAppliedJobs(currentPage, limit), 
+      {
+        headers: {
+        'Accept': 'application/json',
+        }, 
+        withCredentials: true,
+      }
+    );
+  
+    return response.data.data;
+  } catch (error) {
+    throw error.response.data; 
+  }
+}
 
-  return response.data;
-})
+const applyJob = async(jobId) => {
+
+  try {
+    const response = await axios.get(
+      endpoints.userApplyJob(jobId), 
+      {
+        headers: {
+        'Accept': 'application/json',
+        }, 
+        withCredentials: true,
+      }
+    );
+  
+    return response.data;
+  }catch (error) {
+    throw error.response.data; 
+  }
+}
 
 export default {
   getAllJobs,
