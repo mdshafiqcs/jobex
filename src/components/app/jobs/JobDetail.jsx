25ebb4010ddea3bba8d1147b9e68f97005aad1a2
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-27 14:52:38 +0600

    JobDetail component updated

--- src/components/app/jobs/JobDetail.jsx
+++ src/components/app/jobs/JobDetail.jsx
@@ -6,9 +6,20 @@ import { useSelector } from 'react-redux'
 import { ApplyButton } from '.';
 import { helper } from '@/utils';
 import { useSingleJob } from '@/hooks';
+import { cn } from '@/lib/utils';
 
 function JobDetail({job}) {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn) || false;
+
+  const daysLeft = helper.getDaysLeft(job.deadline);
+
+  let daysLeftText = "";
+
+  if (daysLeft === 0) {
+    daysLeftText = "Last day";
+  } else {
+    daysLeftText = `${daysLeft} day${daysLeft > 1 ? 's' : ''} left`;
+  }
 
   return (
     <div className='my-10'>
@@ -19,7 +30,11 @@ function JobDetail({job}) {
           <div className='flex flex-wrap gap-2 my-2'>
             <Badge className='text-cyan-700' variant="outline">{job.positions} Positions</Badge>
             <Badge className='text-cyan-700' variant="outline">{job.jobType}</Badge>
-            <Badge className='text-cyan-700' variant="outline">{job.salary} LPA</Badge>
+            <Badge className='text-cyan-700' variant="outline">
+              {
+                job.isNegotiable ? "Negotiable" : <span>{helper.formatPrice(job.minSalary)} - {helper.formatPrice(job.maxSalary)} Yearly</span>
+              }
+            </Badge>
           </div>
         </div>
         <ApplyButton job={job} />
@@ -33,7 +48,10 @@ function JobDetail({job}) {
         <h1 className='font-bold my-1 text-slate-600'>Role: <span className='pl-4 font-normal text-slate-600'> {job.jobRole || "N/A"} </span>
         </h1>
 
-        <h1 className='font-bold my-1  text-slate-600'>Location: <span className='pl-4 font-normal text-slate-600'> {job.location} </span>
+        <h1 className='font-bold my-1  text-slate-600'>Location: <span className='pl-4 font-normal text-slate-600'> {job.location.name} </span>
+        </h1>
+
+        <h1 className='font-bold my-1  text-slate-600'>Office Address: <span className='pl-4 font-normal text-slate-600'> {job.address} </span>
         </h1>
 
         <h1 className='font-bold my-1  text-slate-600'>Description: <span className='pl-4 font-normal text-slate-600'> {job.description} </span>
@@ -42,13 +60,26 @@ function JobDetail({job}) {
         <h1 className='font-bold my-1  text-slate-600'>Experience: <span className='pl-4 font-normal text-slate-600'> {job.experienceLevel} </span>
         </h1>
 
-        <h1 className='font-bold my-1  text-slate-600'>Salary: <span className='pl-4 font-normal text-slate-600'> {job.salary} LPA </span>
+        <h1 className='font-bold my-1  text-slate-600'>
+          Salary: 
+          <span className='pl-4 font-normal text-slate-600'> 
+            {
+              job.isNegotiable ? "Negotiable" : <span>{helper.formatPrice(job.minSalary)} - {helper.formatPrice(job.maxSalary)} Yearly</span>
+            }
+          </span>
         </h1>
 
         <h1 className='font-bold my-1  text-slate-600'>Total Applicant: <span className='pl-4 font-normal text-slate-600'> {job.applicationCount} </span>
         </h1>
 
         <h1 className='font-bold my-1  text-slate-600'>Posted Date: <span className='pl-4 font-normal text-slate-600'> {helper.getDate(job.createdAt)} </span>
+        </h1>
+
+        <h1 className='font-bold my-1  text-slate-600'>
+          Deadline: 
+          <span className='pl-4'>{helper.getDate(job.deadline)}</span>
+          <span className={cn("pl-2", daysLeft < 3 ? 'text-red-600' : 'text-teal-600')}>( {daysLeftText} )</span>
+
         </h1>
 
       </div>
